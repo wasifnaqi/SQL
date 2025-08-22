d02f5fcefe5401e37bded6c4baf474a804da6408
wasifnaqi <wasifnaqi@hotmail.com>
2025-08-22 17:14:11 +0530

    0001-TWO-SUM Stats: Time: 1269 ms (32.03%), Space: 18.5 MB (71.2%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,6 +1,5 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        flag = False
         for i in range(len(nums)):
             value = target - nums[i]
             for j in range(len(nums)):
@@ -9,12 +8,7 @@ class Solution:
                 else:
                     if nums[j] == value:
                         ans = [i,j]
-                        flag = True
                         ans = ans[::-1]
-                        break
-                if flag:
-                    break
-            if flag:
-                break           
-        return ans    
+                        return ans               
+        return []    
         
